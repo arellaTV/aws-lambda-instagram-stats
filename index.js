65d04b1b1bd24618397144fe3b6910f0ca2ee098
arellaTV <jay@creativealchemy.org>
2022-09-27 09:22:15 -0700

    Switches to instatouch

--- index.js
+++ index.js
@@ -1,73 +1,27 @@
-const axios = require('axios');
-const chromium = require('@sparticuz/chrome-aws-lambda');
-
 const escapeEmoji = (text) => {
   return text.replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, '');
 }
 
-async function getInfoByMediaId(mediaId) {
-  const response = await axios.get(`https://i.instagram.com/api/v1/media/${mediaId}/info/`, {
-    headers: {
-      "accept": "*/*",
-      "accept-language": "en-US,en;q=0.9",
-      "cache-control": "no-cache",
-      "pragma": "no-cache",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
-      "sec-gpc": "1",
-      "x-asbd-id": process.env.ASBD_ID,
-      "x-csrftoken": process.env.CSRF_TOKEN,
-      "x-ig-app-id": process.env.IG_APP_ID,
-      "x-ig-www-claim": process.env.IG_WWW_CLAIM,
-      "x-instagram-ajax": process.env.IG_AJAX,
-      "cookie": process.env.COOKIE,
-      "Referer": "https://www.instagram.com/",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-  });
-
-  return response;
-}
-
 exports.handler = async (event) => {
-  let browser = null;
-
   const notFoundResponse = {
     statusCode: 404,
     body: JSON.stringify('Not found.'),
   };
   try {
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
-    const page = await browser.newPage()
     const url = event.queryStringParameters.url;
-    await page.goto(`view-source:${url}`);
-    const content = await page.content();
-    if (!content) {
-      return notFoundResponse;
-    }
-    const part1 = content.split('instagram://media?id=')[1];
 
-    if (!part1) {
-      return notFoundResponse;
+    if (!url) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify('Bad request.'),
+      };
     }
 
-    const mediaId = part1.split('</span>')[0];
-  
-    if (!mediaId) {
-      return notFoundResponse;
-    }
-  
-    const { data: postMeta } = await getInfoByMediaId(mediaId)
+    const options = { session: process.env.SESSION_ID }
+    const postMeta = await instaTouch.getPostMeta(url, options)
     const postDetails = postMeta.items[0];
 
-    if (!postDetails) {
+    if (!postDetails.items) {
       return notFoundResponse;
     }
 
@@ -100,9 +54,5 @@ exports.handler = async (event) => {
       statusCode: 400,
       body: JSON.stringify('An error occurred.'),
     };
-  } finally {
-    if (browser) {
-      await browser.close();
-    }
   }
 };
